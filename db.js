fafda1ecdcc370ea4c2e84ad81bb93230f630312
Konstantin Ashaev <kostya@gorgaz.ru>
2021-07-21 16:30:25 +0300

    Use || to supply default values for the connection string

--- db.js
+++ db.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+const connectionString = process.env.MONGO_SRV || 'mongodb://localhost:27017/test';
+
+mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
-db.on('error', err => { console.error(err.message) });
+db.on('error', err => {
+  console.error(err.message);
+  process.exit(1);
+});
 db.once('open', () => console.log('Connection established'));
 
