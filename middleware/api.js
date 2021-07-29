75c53cb269589138618fa1472e37124262397571
Konstantin Ashaev <kostya@gorgaz.ru>
2021-07-29 16:16:40 +0300

    Add API for sorting

--- middleware/api.js
+++ middleware/api.js
@@ -1,6 +1,14 @@
 const db = require('../db.js');
 
 exports.getInboxApi = async (req, res) => {
-  const inbox = await db.getInbox();
-  res.json(inbox);
+  const field = req.query.field;
+  const order = req.query.order;
+
+  if (field && order) {
+    const sortedInbox = await db.sortedInbox(field, order);
+    res.json(sortedInbox);
+  } else {
+    const inbox = await db.getInbox();
+    res.json(inbox);
+  }
 }
