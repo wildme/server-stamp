139cd4e116bb9d3723858e015592396e5d618115
Konstantin Ashaev <kostya@gorgaz.ru>
2021-07-30 10:57:39 +0300

    Add parameters to getInbox()

--- middleware/api.js
+++ middleware/api.js
@@ -1,14 +1,9 @@
 const db = require('../db.js');
 
 exports.getInboxApi = async (req, res) => {
-  const field = req.query.field;
-  const order = req.query.order;
+  const field = req.query.field || 'id';
+  const order = req.query.order || 'asc';
+  const inbox = await db.getInbox(field, order);
 
-  if (field && order) {
-    const sortedInbox = await db.sortedInbox(field, order);
-    res.json(sortedInbox);
-  } else {
-    const inbox = await db.getInbox();
-    res.json(inbox);
-  }
+  res.json(inbox);
 }
