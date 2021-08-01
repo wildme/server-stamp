2f3d945f17420fb6bd305f6aa197f1a08ccf0b57
Konstantin Ashaev <techno_nix@mail.ru>
2021-08-01 23:17:46 +0300

    add a new function to handle POST requests

--- middleware/api.js
+++ middleware/api.js
@@ -4,6 +4,11 @@ exports.getInboxApi = async (req, res) => {
   const field = req.query.field || 'id';
   const order = req.query.order || 'asc';
   const inbox = await db.getInbox(field, order);
-
   res.json(inbox);
 }
+
+exports.addInboxApi = async (req, res) => {
+  await db.addInbox(req.body.subject,
+    req.body.fromTo, req.body.addedBy, req.body.notes);
+}
+
