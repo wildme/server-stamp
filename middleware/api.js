ff049119c91e7f0674484233f015aaaa878950f9
Konstantin Ashaev <kostya@gorgaz.ru>
2021-08-03 16:08:56 +0300

    Add middleware functions to get/edit items by their id

--- middleware/api.js
+++ middleware/api.js
@@ -5,10 +5,20 @@ exports.getInboxApi = async (req, res) => {
   const order = req.query.order || 'asc';
   const inbox = await db.getInbox(field, order);
   res.json(inbox);
+};
+exports.getItemByIdApi = async (req, res) => {
+  const box = req.params.box;
+  const id = req.params.id;
+  const item = await db.getItemById(box, id);
+  res.json(item);
 }
 
 exports.addInboxApi = async (req, res) => {
   await db.addInbox(req.body.subject,
     req.body.fromTo, req.body.addedBy, req.body.notes);
-}
+};
 
+exports.editItemApi = async (req, res) => {
+  await db.editInbox(reg.body.id, req.body.subject,
+    req.body.fromTo, req.body.addedBy, req.body.notes);
+};
