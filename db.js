686beed45ef18aefedb3e81dfcb89e87775d2fcd
Konstantin Ashaev <techno_nix@mail.ru>
2021-08-07 22:58:10 +0300

    Create a user if there is none

--- db.js
+++ db.js
@@ -13,13 +13,23 @@ db.once('open', () => console.log('Connection established'));
 const Inbox = require('./models/inbox.js');
 const Outbox = require('./models/outbox.js');
 const LastId = require('./models/lastId.js');
+const User = require('./models/user.js');
 
 LastId.find((err, ids) => {
-  if(err) return console.error(err);
-  if(ids.length) return;
+  if (err) return console.error(err);
+  if (ids.length) return;
 
   new LastId({ box: 'inbox'}).save();
   new LastId({ box: 'outbox'}).save();
+});
+
+User.find((err, user) => {
+  if (err) return console.error(err);
+  if (user.length) return;
+
+  new User({id: 1, login: 'admin', password: 'admin',
+    firstname: 'John', lastname: 'Doe', email: 'admin@example.com',
+    administrator: true}).save();
 });
 
 module.exports = {
