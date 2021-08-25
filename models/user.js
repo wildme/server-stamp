422b5ad8a74e070f6516c672d49fff3123a4cb13
Konstantin Ashaev <kostya@gorgaz.ru>
2021-08-25 16:32:45 +0300

    Rename model fields and values

--- models/user.js
+++ models/user.js
@@ -1,14 +1,15 @@
 const mongoose = require('mongoose');
+//const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = mongoose.Schema({
-  id: Number,
-  login: String,
-  password: String,
+  username: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
   firstname: String,
   lastname: String,
   email: String,
   administrator: {type: Boolean, default: false}
 });
 
+//userSchema.plugin(passportLocalMongoose);
 const User = mongoose.model('User', userSchema);
 module.exports = User;
