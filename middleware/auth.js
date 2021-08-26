ba4a90e1854f3b61dfd058d939a2b97124b4dd68
Konstantin Ashaev <kostya@gorgaz.ru>
2021-08-26 16:19:51 +0300

    Add req.login() to passport.authenticate(). testing

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-const db = require('../db.js');
 const User = require('../models/user.js');
-
-//passport.use(User.createStrategy());
-//passport.use(new LocalStartegy(User.authenticate()));
-//passport.serializeUser(User.serializeUser());
-//passport.deserializeUser(User.deserializeUser());
-
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    User.findOne({ username: username }, function(err, user) {
-      if (err) return done(err);
-      if (!user) { 
-        return done(null, false, { message: 'Incorrect username' })
-      }
-      if (password === user.password) {
-        return done(null, user);
-      } else {
-        return done(null, false, { message: 'Incorrect password' });
-      }
-    });
-  }
-));
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
@@ -31,18 +9,37 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(id, done) {
   User.findById(id, function(err, user) {
     done(err, user);
-  });
+    });
 });
 
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function(err, user) {
+     if (err) return done(err);
+     if (!user) {
+       return done(null, false, { message: 'Incorrect username' });
+       }
+      if (password === user.password) {
+        return done(null, user);
+        } else {
+          return done(null, false, { message: 'Incorrect password' });
+          }
+      });
+    }
+  ));
+
 exports.init = (app) => {
-    app.use(passport.initialize());
-    app.use(passport.session());
-  };
-
-exports.loginApi = (req, res) => {
-  passport.authenticate('local', { successRedirect: '/',
-    failureRedirect: '/login' })
+  app.use(passport.initialize());
+  app.use(passport.session());
 };
-    
 
-
+exports.loginApi = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) return next(err);
+    if (!user)  return res.redirect('/login');
+    req.logIn(user, (err) => {
+      if (err)  return next(err);
+      return res.redirect('/');
+      })
+    })(req, res, next);
+};
