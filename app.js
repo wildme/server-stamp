a21ad247bf1d3957534c09647da00a7a2e36abc7
Konstantin Ashaev <kostya@gorgaz.ru>
2021-08-27 16:14:25 +0300

    Disable cookie-parser

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const session = require('express-session');
-const cookieparser = require('cookie-parser');
+//const cookieparser = require('cookie-parser');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
@@ -13,7 +13,7 @@ app.use('/api', cors());
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(cookieparser());
+//app.use(cookieparser());
 app.use(session({
   name: 'session-id',
   secret: '123-456-789',
