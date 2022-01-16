79787218a19458d37ceaec85cb9dd128bc7c5df8
Konstantin Ashaev <techno_nix@mail.ru>
2022-01-16 22:01:34 +0300

    The await key word is added in front of mongoose queries. Check for new year is added

--- db.js
+++ db.js
@@ -17,35 +17,38 @@ const User = require('./models/user.js');
 const Contact = require('./models/contact.js');
 
 module.exports = {
-  getBox: async (page, field, order) =>
+  getItems: async (page, field, order) =>
   page === 'inbox' ?
-  Inbox.find({}).sort([[field, order]]) :
-  Outbox.find({}).sort([[field, order]]),
+  await Inbox.find({}).sort([[field, order]]) :
+  await Outbox.find({}).sort([[field, order]]),
 
   getItemById: async (page, id) =>
     page === 'inbox' ?
-  Inbox.find({id: id}) :
-  Outbox.find({id: id}),
+  await Inbox.find({id: id}) :
+  await Outbox.find({id: id}),
 
   updateItemById: async(id, page, subject, fromTo, notes) =>
   page === 'inbox' ?
   Inbox.updateOne({id: id}, {subject: subject, from: fromTo, notes: notes}) :
   Outbox.updateOne({id: id}, {subject: subject, to: fromTo, notes: notes}),
 
-  addBox: async (page, subject, fromTo, addedBy, notes) => {
-    await LastId.updateOne({box: page}, {$inc: {lastId: 1}});
-    const id = await LastId.findOne({box: page}, 'lastId')
+  addItem: async (page, subject, fromTo, addedBy, notes) => {
+    const year = new Date().getFullYear();
+    const docForCurrentYear = await LastId.findOne({box: page, year: year});
+    if (!(docForCurrentYear)) new LastId({box: page, year: year}).save();
+    await LastId.updateOne({box: page, year: year}, {$inc: {lastId: 1}});
+    const id = await LastId.findOne({box: page, year: year}, 'lastId');
     const doc = page === 'inbox' ?
-      new Inbox({ id: id.lastId, from: fromTo, subject: subject,
+      new Inbox({ id: [id.lastId, year].join('-'), from: fromTo, subject: subject,
         addedBy: addedBy, notes: notes, date: new Date }) :
-      new Outbox({ id: id.lastId, to: fromTo, subject: subject,
+      new Outbox({ id: [id.lastId, year].join('-'), to: fromTo, subject: subject,
       addedBy: addedBy, notes: notes, date: new Date });
-    doc.save();
+    await doc.save();
   },
 
-  checkUsername: async (username) => User.findOne({username: username}, 'username'),
+  checkUsername: async (username) => await User.findOne({username: username}, 'username'),
 
-  checkEmail: async (email) => User.findOne({email: email}, 'email'),
+  checkEmail: async (email) => await User.findOne({email: email}, 'email'),
 
   signup: async (username, password, firstname, lastname, email) => {
     new User({username: username, password: password,
@@ -56,7 +59,7 @@ module.exports = {
 
   searchContactsByName: async (name) => {
   const string  = new RegExp(name, "i");
-  return Contact.find({name: string}, 'name location').exec();
+  return await Contact.find({name: string}, 'name location').exec();
   },
 
   addContact: async (location, region, name) => {
