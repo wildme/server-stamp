1fc774628f6c2101546c7a29bc494aad622270b2
Konstantin Ashaev <techno_nix@mail.ru>
2022-01-16 21:55:25 +0300

    Change the order of endpoints in the file

--- app.js
+++ app.js
@@ -24,19 +24,19 @@ app.use(session({
 const port =  process.env.PORT || 3001;
 auth.init(app);
 
-app.post('/api/verify/token', auth.verifyTokenApi);
-app.post('/api/login', auth.loginApi);
 app.get('/api/refresh/token', auth.refreshTokenApi);
 app.get('/api/logout', auth.logoutApi);
 app.get('/api/token', auth.refreshTokenApi);
 app.get('/api/contacts', api.getContactsApi);
 app.get('/api/contacts/search/by-name', api.searchContactsByNameApi);
-app.get('/api/:box', api.getBoxApi);
+app.get('/api/:box', api.getItemsApi);
 app.get('/api/:box/:id', api.getItemByIdApi);
-app.post('/api/contacts/new', api.addContactApi);
-app.post('/api/:box/new', api.addBoxApi);
-app.post('/api/:box/update/:id', api.updateItemByIdApi);
+app.post('/api/verify/token', auth.verifyTokenApi);
+app.post('/api/login', auth.loginApi);
 app.post('/api/signup', api.signupApi);
+app.post('/api/contacts/new', api.addContactApi);
+app.post('/api/:box/new', api.addItemApi);
+app.post('/api/:box/update/:id', api.updateItemByIdApi);
 
 app.listen(port, () => console.log(
   `Express is running at http://localhost:${port}`));
