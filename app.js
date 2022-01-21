a749cae5e82bc51f11639b356e873e6887986672
Konstantin Ashaev <kostya@gorgaz.ru>
2022-01-21 15:58:35 +0300

    uploadFileApi(): Route parametres added

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post('/api/login', auth.loginApi);
 app.post('/api/signup', api.signupApi);
 app.post('/api/contacts/new', api.addContactApi);
 app.post('/api/:box/new', api.addItemApi);
-app.post('/api/upload', api.uploadFileApi);
+app.post('/api/:box/upload/:id', api.uploadFileApi);
 app.post('/api/:box/update/:id', api.updateItemByIdApi);
 app.post('/api/:box/status/:id', api.updateStatusApi);
 
