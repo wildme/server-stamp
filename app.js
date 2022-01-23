fcf1d6feca47332fc7d18dbc7c28034949873811
Konstantin Ashaev <techno_nix@mail.ru>
2022-01-23 20:47:20 +0300

    New endpoints added

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const session = require('express-session');
 const cookieparser = require('cookie-parser');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+
+const upload = multer({ dest: 'files/'
+  + new Date().getFullYear()
+  + '/' + new Date().getMonth(),
+  limits: { fileSize: 5120 }
+});
 
 const api = require('./middleware/api.js');
 require('./db.js');
@@ -29,14 +36,16 @@ app.get('/api/logout', auth.logoutApi);
 app.get('/api/token', auth.refreshTokenApi);
 app.get('/api/contacts', api.getContactsApi);
 app.get('/api/contacts/search/by-name', api.searchContactsByNameApi);
+app.get('/api/download/:file', api.downloadFileApi);
 app.get('/api/:box', api.getItemsApi);
 app.get('/api/:box/:id', api.getItemByIdApi);
+app.get('/api/attachment/:box/:id', api.getAttachmentByIdApi);
 app.post('/api/verify/token', auth.verifyTokenApi);
 app.post('/api/login', auth.loginApi);
 app.post('/api/signup', api.signupApi);
 app.post('/api/contacts/new', api.addContactApi);
 app.post('/api/:box/new', api.addItemApi);
-app.post('/api/:box/upload/:id', api.uploadFileApi);
+app.post('/api/:box/upload/:id', upload.single('file'), api.uploadFileApi);
 app.post('/api/:box/update/:id', api.updateItemByIdApi);
 app.post('/api/:box/status/:id', api.updateStatusApi);
 
