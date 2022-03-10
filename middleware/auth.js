b51802f978ba93d088645ed9f285e2f4168803f5
Konstantin Ashaev <kostya@gorgaz.ru>
2022-03-10 16:37:17 +0300

    Replace some strings with env. variables. Change formatting

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,11 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/user.js');
 const bcrypt = require('bcrypt');
 
+const jwtSecret = process.env.STAMP_JWT_SECRET;
+const jwtCookieAge = Number(process.env.STAMP_JWT_COOKIE_AGE);
+const jwtAccessExp = Number(process.env.STAMP_JWT_ACCESS_EXP);
+const jwtRefreshExp = Number(process.env.STAMP_JWT_REFRESH_EXP);
+
 passport.serializeUser(function(user, done) {
   done(null, user.id);
 });
@@ -13,37 +18,39 @@ passport.serializeUser(function(user, done) {
 passport.deserializeUser(function(id, done) {
   User.findById(id, function(err, user) {
     done(err, user);
-    });
+  });
 });
 
-passport.use(new JwtStrategy({
-  jwtFromRequest: function(req) {
-    let refreshToken = null;
-    if (req && req.cookies) refreshToken = req.cookies['jwt'];
-    return refreshToken;
+passport.use(new JwtStrategy({ jwtFromRequest: function(req) {
+  let refreshToken = null;
+  if (req && req.cookies) refreshToken = req.cookies['jwt'];
+
+  return refreshToken;
   },
-  secretOrKey: 'my_secret'
+  secretOrKey: jwtSecret
 },
   function(jwt_payload, done) {
     User.findById(jwt_payload.sub, function(err, user) {
-      if (err) { return done(err, false); }
-      if (user) { return done(null, user); }
-      else { return done(null, false); }
+      if (err) return done(err, false);
+      if (user) return done(null, user);
+
+      return done(null, false);
     });
-  }));
+  })
+);
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
     User.findOne({ username: username }, function(err, user) {
-     if (err) return done(err);
-     if (!user) return done(null, false, { message: 'Incorrect username' });
+      if (err) return done(err);
+      if (!user) return done(null, false, { message: 'Incorrect username' });
       bcrypt.compare(password, user.password, function(err, result) {
         if (result) return done(null, user);
         else return done(null, false, { message: 'Incorrect password' });
       });
     })
   }
-  ));
+));
 
 exports.init = (app) => {
   app.use(passport.initialize());
@@ -67,31 +74,34 @@ exports.loginApi = (req, res, next) => {
       if (err)  return next(err);
       const accessToken_payload = {
         sub: user._id,
-        exp: Math.floor(Date.now() / 1000) + 600,
+        exp: Math.floor(Date.now() / 1000) + jwtAccessExp,
         username: user.username,
         admin: user.administrator
       };
       const refreshToken_payload = {
         sub: user._id,
-        exp: Math.floor(Date.now() / 1000) + 28800,
+        exp: Math.floor(Date.now() / 1000) + jwtRefreshExp,
         username: user.username,
         admin: user.administrator
       };
-      const profile = { username: user.username, admin: user.administrator,
-        fullname: [user.firstname, user.lastname].join(' '), email: user.email };
-      const accessToken = jwt.sign(accessToken_payload, 'my_secret');
+      const profile = {
+        username: user.username,
+        admin: user.administrator,
+        fullname: [user.firstname, user.lastname].join(' '),
+        email: user.email
+      };
+      const accessToken = jwt.sign(accessToken_payload, jwtSecret);
       const refreshToken = undefined;
 
       if (!req.cookies['jwt']) {
-        let refreshToken = jwt.sign(refreshToken_payload, 'my_secret');
+        let refreshToken = jwt.sign(refreshToken_payload, jwtSecret);
 
         return res.status(200)
-        .cookie('jwt', refreshToken, { httpOnly: true, maxAge: 28800000 })
+        .cookie('jwt', refreshToken, { httpOnly: true, maxAge: jwtCookieAge })
         .json({ user: profile, token: accessToken });
       }
 
-      return res.status(200)
-        .json({ user: profile, token: accessToken });
+      return res.status(200).json({ user: profile, token: accessToken });
       })
     })(req, res, next);
 };
@@ -101,22 +111,25 @@ exports.refreshTokenApi = (req, res, next) => {
     if (err) return next(err);
     if (!user) return res.status(401).send();
     const accessToken_payload = {
-        sub: user._id,
-        exp: Math.floor(Date.now() / 1000) + 600,
-        username: user.username,
-        admin: user.administrator
-      };
-    const profile = { username: user.username, admin: user.administrator,
-      fullname: [user.firstname, user.lastname].join(' '), email: user.email };
-    const accessToken = jwt.sign(accessToken_payload, 'my_secret');
+      sub: user._id,
+      exp: Math.floor(Date.now() / 1000) + jwtAccessExp,
+      username: user.username,
+      admin: user.administrator
+    };
+    const profile = {
+      username: user.username,
+      admin: user.administrator,
+      fullname: [user.firstname, user.lastname].join(' '),
+      email: user.email
+    };
+    const accessToken = jwt.sign(accessToken_payload, jwtSecret);
 
-    return res.status(200)
-      .json({ user: profile, token: accessToken });
+    return res.status(200).json({ user: profile, token: accessToken });
   })(req, res, next);
 };
 
 exports.verifyTokenApi = (req, res) => {
-  jwt.verify(req.body.token, 'my_secret', (err, decoded) => {
+  jwt.verify(req.body.token, jwtSecret, (err, decoded) => {
     if (err) {
       if (err.message === 'jwt expired') {
         return res.status(401).send();
