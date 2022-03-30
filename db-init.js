6752ec41bc0b252d596295aa2d8a6e1e48e9bb54
Konstantin Ashaev <kostya@gorgaz.ru>
2022-03-30 13:10:34 +0300

    Change admin defaults. Delete spaces after/before curly braces

--- db-init.js
+++ db-init.js
@@ -15,17 +15,23 @@ const LastId = require('./models/lastId.js');
 const User = require('./models/user.js');
 const Settings = require('./models/settings.js');
 
-const admin = new User({ username: 'admin', password: 'admin',
-      firstname: 'John', lastname: 'Doe', email: 'admin@example.com',
-      administrator: true });
-const inboxLastId = new LastId({ box: 'inbox' });
-const outboxLastId = new LastId({ box: 'outbox' });
-const language = new Settings({ language: process.env.STAMP_LANG || 'en-En' });
+const admin = new User({
+  username: 'admin',
+  password: 'admin',
+  firstname: 'Admin',
+  lastname: 'Admin',
+  email: 'admin@example.com',
+  administrator: true
+});
+
+const inboxLastId = new LastId({box: 'inbox'});
+const outboxLastId = new LastId({box: 'outbox'});
+const language = new Settings({language: process.env.STAMP_LANG || 'en-En'});
 
 (async function() {
-  const adminUser = await User.exists({ username: 'admin' });
-  const inLastId = await LastId.exists({ box: 'inbox' });
-  const outLastId = await LastId.exists({ box: 'outbox' });
+  const adminUser = await User.exists({username: 'admin'});
+  const inLastId = await LastId.exists({box: 'inbox'});
+  const outLastId = await LastId.exists({box: 'outbox'});
   const appLanguage = await Settings.findOne({'language': { $exists: true }});
 
   if (adminUser) { 
