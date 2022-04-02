1cb7bf29353f55db9846f4043ccb346bbe3358b4
Konstantin Ashaev <konstantin.ashaev@gmail.com>
2022-04-02 21:21:10 +0300

    Add an enum validator

--- models/lastId.js
+++ models/lastId.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const lastIdSchema = mongoose.Schema({
-  box: { type: String }, 
+  box: { type: String, enum: ['inbox', 'outbox'] },
   lastId: { type: Number, default: 0 },
   year: { type: Date, default: new Date().getFullYear() }
 });
