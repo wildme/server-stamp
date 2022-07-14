3f28082d34ebb26d5c8d4e2035f2ffbfd558273a
Konstantin Ashaev <kostya@gorgaz.ru>
2022-07-14 15:51:50 +0300

    Add a required validator

--- models/userSettings.js
+++ models/userSettings.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const userSettingsSchema = mongoose.Schema({
-  username: { type: String, unique: true },
+  username: { type: String, unique: true, required: true },
   settings: {
     records: {
       sortOrder: { type: String, enum: ['desc', 'asc'], default: 'asc' }
