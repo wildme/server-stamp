39bebe8965ec5621cfdd942355b147c4160a6958
Konstantin Ashaev <kostya@gorgaz.ru>
2022-10-20 16:30:04 +0300

    Change the data type of the year field

--- models/lastId.js
+++ models/lastId.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const lastIdSchema = mongoose.Schema({
   box: { type: String, enum: ['inbox', 'outbox'] },
   lastId: { type: Number, default: 0 },
-  year: { type: Date, default: new Date().getFullYear() }
+  year: { type: String }
 });
 
 const lastId = mongoose.model('LastId', lastIdSchema);
