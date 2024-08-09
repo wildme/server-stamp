20ddbb9aa4a0258d3a17d1af1d1e8370af5292d2
Konstantin Ashaev <kostya@gorgaz.ru>
2024-08-09 09:56:46 +0300

    Add support for directives

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const sessionOpts = {secret: sessionSecret, resave: false,
   saveUninitialized: true};
 const appDirs = [staticDir, uploadDir];
 const wss = new WebSocketServer({ port: wssPort });
-let sockets = {inbox:[], outbox:[]};
+let sockets = {inbox:[], outbox:[], directive:[]};
 
 for (let i = 0; i < appDirs.length; i++) {
   access(appDirs[i], constants.F_OK, (err) => {
@@ -42,18 +42,19 @@ process.on('SIGINT', () => {
 });
 
 wss.on('connection', function connection(ws, req) {
-  const box = req.url.slice(1);
-  if (box === 'inbox') sockets.inbox.push(ws);
-  if (box === 'outbox') sockets.outbox.push(ws);
+  const doc = req.url.slice(1);
+  if (doc === 'inbox') sockets.inbox.push(ws);
+  if (doc === 'outbox') sockets.outbox.push(ws);
+  if (doc === 'directive') sockets.directive.push(ws);
   ws.on('message', function message(data) {
-    sockets[box].forEach(function(socket) {
+    sockets[doc].forEach(function(socket) {
       if (socket !== ws && socket.readyState === WebSocket.OPEN) {
         socket.send(data.toString());
       }
     });
   });
   ws.on('close', function() {
-    sockets[box] = sockets[box].filter(s => s !== ws);
+    sockets[doc] = sockets[doc].filter(s => s !== ws);
   });
 });
 
@@ -69,26 +70,34 @@ login.init(app);
 app.get('/api/page-reload', token.onPageReload);
 app.get('/api/logout', login.logoutApi);
 app.get('/api/contacts', token.authenticate, api.getContactsApi);
-app.get('/api/download/:file', token.authenticate, api.downloadFileApi);
 app.get('/api/get/language', api.getAppLanguageApi);
-app.get('/api/:box', token.authenticate, api.getItemsApi);
+app.get('/api/get/codes/:doc', token.authenticate, api.getDocCodesApi);
+app.get('/api/get/:doc', token.authenticate, api.getDirectivesApi);
+app.get('/api/edit/directive/:id', token.authenticate, api.fetchDirectiveByIdApi);
+app.get('/api/view/directive/:id', token.authenticate, api.getDirectiveByIdApi);
+app.get('/api/get/:doc/nextid', token.authenticate, api.getNextDocIdApi);
+app.get('/api/:doc', token.authenticate, api.getItemsApi);
 app.get('/api/:box/nextid', token.authenticate, api.getNextRecordIdApi);
 app.get('/api/edit/:box/:id', token.authenticate, api.fetchItemByIdApi);
 app.get('/api/view/:box/:id', token.authenticate, api.getItemByIdApi);
+app.get('/api/:doc/download/:file', token.authenticate, api.downloadFileApi);
 app.get('/*', api.getReactIndex);
 app.post('/api/login', login.loginApi);
 app.post('/api/signup', api.signupApi);
 app.post('/api/contacts/new', token.authenticate, api.addContactApi);
 app.post('/api/reset/password', api.resetPasswordApi);
+app.post('/api/directive/new', token.authenticate, api.addDirectiveApi);
+app.post('/api/directive/upload', token.authenticate, getFile.uploadFileApi);
 app.post('/api/:box/new', token.authenticate, api.addItemApi);
-app.post('/api/:box/upload', token.authenticate, getFile.uploadFileApi);
 app.post('/api/:box/search-by-id', token.authenticate, api.searchRecordsByIdApi);
+app.post('/api/:doc/upload', token.authenticate, getFile.uploadFileApi);
 app.put('/api/user/update/info', token.authenticate, api.updateUserInfoApi);
 app.put('/api/user/update/email', token.authenticate, api.updateUserEmailApi);
 app.put('/api/user/update/password', token.authenticate, api.updateUserPasswordApi);
 app.put('/api/user/update/settings', token.authenticate, api.updateUserSettingsApi);
 app.put('/api/contact/update/:id', token.authenticate, api.updateContactByIdApi);
-app.put('/api/:box/status/:id', token.authenticate, api.updateStatusApi);
+app.put('/api/directive/update/:id', token.authenticate, api.updateDirectiveByIdApi);
+app.put('/api/:doc/status/:id', token.authenticate, api.updateStatusApi);
 app.put('/api/:box/update/:id', token.authenticate, api.updateItemByIdApi);
-app.delete('/api/attachment/delete/:id', token.authenticate, api.deleteAttachmentByNameApi);
+app.delete('/api/:doc/attachment/delete/:id', token.authenticate, api.deleteAttachmentByNameApi);
 app.delete('/api/contact/delete/:id', token.authenticate, api.deleteContactByIdApi);
