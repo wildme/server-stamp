c08eb2ffa8d581fea179b9837b0c3651d20114aa
Konstantin Ashaev <kostya@gorgaz.ru>
2024-08-12 09:21:14 +0300

    Update db checks

--- db-init.js
+++ db-init.js
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 
 const mongoose = require('mongoose');
-const lastId = require('./models/lastId.js');
-const user = require('./models/user.js');
-const settings = require('./models/settings.js');
+const LastId = require('./models/lastId.js');
+const User = require('./models/user.js');
+const Settings = require('./models/settings.js');
+const DocCode = require('./models/docCode.js');
+
+const db = mongoose.connection;
 const connectionString = process.env.STAMP_MONGODB || 'mongodb://localhost:27017/stamp';
 const opts = {connectTimeoutMS: 5000};
 const curYear = String(new Date().getFullYear());
 
 mongoose.connect(connectionString, opts)
-  .catch((err) => {console.error("Couldn't init connection to MongoDB: ", err)});
-const db = mongoose.connection;
+  .catch((err) => {console.error("Enable connect to MongoDB: ", err)});
 
 db.once('open', () => console.log('Connection established'));
 db.on('error', (err) => {console.error(err.message); process.exit(1);})
@@ -18,17 +20,23 @@ db.on('error', (err) => {console.error(err.message); process.exit(1);})
   .on('disconnected', () => console.log('Disconnected from MongoDB'));
 
 (async function() {
-  const adminUser = await user.exists({username: 'admin'});
-  const inLastId = await lastId.exists({box: 'inbox'});
-  const outLastId = await lastId.exists({box: 'outbox'});
-  const appLang = await settings.findOne({'language': { $exists: true }});
+  const adminUser = await User.exists({username: 'admin'});
+  const inLastId = await LastId.exists({box: 'inbox'});
+  const outLastId = await LastId.exists({box: 'outbox'});
+  const appLang = await Settings.findOne({'language': { $exists: true }});
+  const docCodes = await DocCode.exists();
 
   if (adminUser) { 
     console.log('Admin account exists. Skipping');
   } else {
-    const admin = new User({username: 'admin', password: 'admin',
-      firstname: 'Admin', lastname: 'Admin', email: 'admin@example.com',
-      administrator: true});
+    const admin = new User({
+      username: 'admin',
+      password: 'admin',
+      firstname: 'Admin',
+      lastname: 'Admin',
+      email: 'admin@example.com',
+      administrator: true
+    });
     await admin.save()
       .then(() => {console.log('Admin account has been created!');})
       .catch((err) => {console.error(err)});
@@ -36,7 +44,10 @@ db.on('error', (err) => {console.error(err.message); process.exit(1);})
   if (inLastId) {
     console.log('Inbox lastID collection exists. Skipping');
   } else {
-    const inboxLastId = new lastId({box: 'inbox', year: curYear});
+    const inboxLastId = new LastId({
+      box: 'inbox',
+      year: curYear
+    });
     await inboxLastId.save()
       .then(() => {console.log('Inbox lastId has been created');})
       .catch((err) => {console.error(err)});
@@ -44,15 +55,32 @@ db.on('error', (err) => {console.error(err.message); process.exit(1);})
   if (outLastId) {
     console.log('Outbox lastID collection exists. Skipping');
   } else {
-    const outboxLastId = new lastId({box: 'outbox', year: curYear});
+    const outboxLastId = new LastId({
+      box: 'outbox',
+      year: curYear
+    });
     await outboxLastId.save()
       .then(() => {console.log('Outbox lastId has been created');})
       .catch((err) => {console.error(err)});
   }
+  if (docCodes === null) {
+    const docCodeDefault = new DocCode({
+      doc: 'directive',
+      code: 'MN',
+      title: 'Main activity'
+    });
+    await docCodeDefault.save()
+      .then(() => {console.log('Document codes have been created');})
+      .catch((err) => {console.error(err)});
+  } else {
+    console.log('Document codes exists. Skipping');
+  }
   if (appLang) {
     console.log('Language is already set. Skipping');
   } else {
-    const lang = new settings({language: process.env.STAMP_LANG || 'en-En'});
+    const lang = new Settings({
+      language: process.env.STAMP_LANG || 'en-En'
+    });
     await lang.save()
       .then(() => {console.log('Language has been set');})
       .catch((err) => {console.error(err)});
