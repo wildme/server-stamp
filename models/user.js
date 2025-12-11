2544ac79d566abe30c7ace7094ba2e91d88349d2
Konstantin Ashaev <kostya@gorgaz.ru>
2025-12-11 10:24:45 +0300

    Rewrite the pre hook according to Mongoose 9 API

--- models/user.js
+++ models/user.js
@@ -21,16 +21,10 @@ const userSchema = mongoose.Schema({
   }
 });
 
-userSchema.pre('save', function(next) {
+userSchema.pre('save', async function() {
   const user = this;
-  bcrypt.genSalt(saltRounds, function(err, salt) {
-    if (err) return next(err);
-    bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
-      user.password = hash;
-      next();
-    });
-  });
+  const hash = await bcrypt.hash(user.password, saltRounds);
+  user.password = hash;
 });
 
 const User = mongoose.model('User', userSchema);
